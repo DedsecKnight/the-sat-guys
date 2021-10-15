dd80969c7fd49d6e2e4afcbfa8e431248c305281
DedsecKnight <mike.nguyen@acmutd.co>
2021-10-15 14:12:06 -0500

    fix bug in navbar

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -9,26 +9,26 @@ interface Props {
 
 const NavItem: React.FC<Props> = ({ active, icon, title, redirect }) => {
     return (
-        <div
-            className={`p-3 flex flex-row gap-x-2 items-center cursor-pointer ${
-                active ? "bg-green-500" : "hover:bg-green-300"
-            } rounded-xl mb-2`}
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke={active ? "white" : "currentColor"}
+        <Link href={redirect}>
+            <div
+                className={`p-3 flex flex-row gap-x-2 items-center cursor-pointer ${
+                    active ? "bg-green-500" : "hover:bg-green-300"
+                } rounded-xl mb-2`}
             >
-                {icon}
-            </svg>
-            <Link href={redirect}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke={active ? "white" : "currentColor"}
+                >
+                    {icon}
+                </svg>
                 <h1 className={active ? "text-white" : "text-black"}>
                     {title}
                 </h1>
-            </Link>
-        </div>
+            </div>
+        </Link>
     );
 };
 
