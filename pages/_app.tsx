7462928811ad033e8e80cc9c3c23b19ded51b84b
DedsecKnight <mike.nguyen@acmutd.co>
2021-12-22 23:26:58 -0600

    refactor: Re-organize code to show header on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import Hamburger from "../components/Hamburger";
 import { NavContextProvider } from "../components/NavContext";
+import SearchBar from "../components/SearchBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NavContextProvider>
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center w-full gap-x-6">
+          <Hamburger />
+          <SearchBar />
+        </div>
+        <div>
+          <h1 className="text-xl font-semibold">Admin</h1>
+        </div>
+      </div>
       <Component {...pageProps} />
     </NavContextProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
-import Hamburger from "../components/Hamburger";
 import { useNavContext } from "../components/NavContext";
 import RecentDonation from "../components/RecentDonation";
 import RecentExam from "../components/RecentExam";
-import SearchBar from "../components/SearchBar";
 
 export default function HomePage() {
   const { updateEndpoint } = useNavContext();
@@ -12,21 +10,10 @@ export default function HomePage() {
   }, []);
 
   return (
-    <>
-      <div className="flex flex-row items-center justify-between">
-        <div className="flex flex-row items-center w-full gap-x-6">
-          <Hamburger />
-          <SearchBar />
-        </div>
-        <div>
-          <h1 className="text-xl font-semibold">Admin</h1>
-        </div>
-      </div>
-      <div className="my-10">
-        <h1 className="text-3xl font-bold">Welcome, Admin</h1>
-        <RecentExam />
-        <RecentDonation />
-      </div>
-    </>
+    <div className="my-10">
+      <h1 className="text-3xl font-bold">Welcome, Admin</h1>
+      <RecentExam />
+      <RecentDonation />
+    </div>
   );
 }
