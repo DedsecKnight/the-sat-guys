3ecdf943e8396ceb49f49f1b732e9b04dbacfb8b
DedsecKnight <mike.nguyen@acmutd.co>
2022-01-04 17:30:19 -0600

    feat(donate): added feature to show uploaded image on step 3

--- components/donate/FRView.tsx
+++ components/donate/FRView.tsx
@@ -1,4 +1,6 @@
 import { QuestionConfig } from "../../interfaces/QuestionConfig";
+import Image from "next/image";
+import PreviewEntry from "./PreviewEntry";
 
 interface FRViewProps {
   questionConfig: QuestionConfig;
@@ -7,7 +9,10 @@ interface FRViewProps {
 export default function FRView({ questionConfig }: FRViewProps) {
   return (
     <div className="border-2 rounded-xl py-3 px-7 my-8 xl:w-4/5 mx-auto">
-      <h1 className="text-xl">{questionConfig.question.question}</h1>
+      <PreviewEntry
+        text={questionConfig.question.question}
+        image={questionConfig.question.image}
+      />
       <div className="p-6">
         <h1 className="text-lg">Type your answer here: ______________</h1>
       </div>
--- components/donate/MCView.tsx
+++ components/donate/MCView.tsx
@@ -1,4 +1,6 @@
 import { QuestionConfig } from "../../interfaces/QuestionConfig";
+import Image from "next/image";
+import PreviewEntry from "./PreviewEntry";
 
 interface MCViewProps {
   questionConfig: QuestionConfig;
@@ -7,15 +9,32 @@ interface MCViewProps {
 export default function MCView({ questionConfig }: MCViewProps) {
   return (
     <div className="border-2 rounded-xl py-3 px-7 my-8 xl:w-4/5 mx-auto">
-      <h1 className="text-xl">{questionConfig.question.question}</h1>
+      <div className="w-full">
+        <PreviewEntry
+          text={questionConfig.question.question}
+          image={questionConfig.question.image}
+        />
+      </div>
       <div className="p-4 flex flex-col">
-        <div className="flex flex-row justify-around my-1">
-          <h1 className="text-lg">A. {questionConfig.answers[0].answer}</h1>
-          <h1 className="text-lg">B. {questionConfig.answers[1].answer}</h1>
+        <div className="flex flex-row justify-around my-1 gap-x-4">
+          <PreviewEntry
+            text={`A. ${questionConfig.answers[0].answer}`}
+            image={questionConfig.answers[0].image}
+          />
+          <PreviewEntry
+            text={`B. ${questionConfig.answers[1].answer}`}
+            image={questionConfig.answers[1].image}
+          />
         </div>
-        <div className="flex flex-row justify-around my-1">
-          <h1 className="text-lg">C. {questionConfig.answers[2].answer}</h1>
-          <h1 className="text-lg">D. {questionConfig.answers[3].answer}</h1>
+        <div className="flex flex-row justify-around my-1 gap-x-4">
+          <PreviewEntry
+            text={`C. ${questionConfig.answers[2].answer}`}
+            image={questionConfig.answers[2].image}
+          />
+          <PreviewEntry
+            text={`D. ${questionConfig.answers[3].answer}`}
+            image={questionConfig.answers[3].image}
+          />
         </div>
       </div>
     </div>
