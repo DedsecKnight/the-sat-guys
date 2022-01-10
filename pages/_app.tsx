c1318a3efb4fdf44cf0827d5a625b6d1376bfa9e
Mike Nguyen <mikenguyen7268@gmail.com>
2022-01-10 00:03:22 -0600

    chore: temporarily disable AWS amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "tailwindcss/tailwind.css";
 import Hamburger from "../components/utilities/Hamburger";
 import { NavContextProvider } from "../components/context-api/NavContext";
 import SearchBar from "../components/utilities/SearchBar";
-import "../lib/aws-config";
 import { NotificationContextProvider } from "../components/context-api/NotificationContext";
 import NotificationList from "../components/utilities/NotificationList";
 import { LoadingContextProvider } from "../components/context-api/LoadingContext";
